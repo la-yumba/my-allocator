108aaed87a5fff28b9beb8517a0e7c4be87b8342
montevideohostel <info@montevideohostel.com>
2015-05-12 11:09:09 +0000

    should work with no config passed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var DEFAULT_BASE_URL = 'https://api.myallocator.com/pms/v201408/json/';
 var API_METHODS = [
   'ARIRulesList',
   'ARIRulesUpdate',
@@ -25,8 +26,10 @@ var API_METHODS = [
 ];
 
 module.exports = function(config, request) {
+  var baseUrl = config && config.baseUrl || DEFAULT_BASE_URL;
+  var authParams = config && config.authParams || {};
+  
   request = request || require('request');
-  var baseUrl = config.baseUrl || 'https://api.myallocator.com/pms/v201408/json/';
   
   var api = {
     DATE_FORMAT: 'YYYY-MM-DD'
@@ -45,7 +48,7 @@ module.exports = function(config, request) {
       url: baseUrl + method,
       method: 'POST',
       json: true,
-      body: _.merge({}, config.authParams || {}, requestParams)
+      body: _.merge({}, authParams, requestParams)
     };
     
     request.post(requestOptions, function(err, res, body) {
